ae9c61c0c88ac0ef6fb9ae829da42ad7b435795f
Hoang Dinh Hieu <hieuhoangatm@gmail.com>
2023-02-26 10:33:33 +0700

    Update DSA02010-TapHopSoCoTongBangX.cpp

--- DSA-PTIT/DSA02010-TapHopSoCoTongBangX.cpp
+++ DSA-PTIT/DSA02010-TapHopSoCoTongBangX.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a[25],ok,c[25],n,x;
-
+int a[25],c[25],n,x;
+int ok;
 void in(int m){
     cout <<"[";
     for(int i=1;i<m;i++) cout << c[i] << " ";
@@ -14,15 +14,13 @@ void quaylui(int i,int s){
     for(j=1;j<=n;j++){
         if(a[j] >= c[i-1] && s+a[j]<=x){
             c[i]=a[j];
-            s+=a[j];
+            s=s+a[j];
             if(s==x){
                 ok=1;
                 in(i);
             }
-            else{
-                quaylui(i+1,s);
-            }
-            s-=a[j];
+            else quaylui(i+1,s);
+            s=s-a[j];
         }
     }
 }
@@ -32,7 +30,7 @@ int main(){
     cin >> t;
     while(t--){
         cin >> n >> x;
-        for(int i=1;i<=n;i++) cin >> a[i];
+        for(i=1;i<=n;i++) cin >> a[i];
         c[0]=0;ok=0;
         quaylui(1,0);
         if(!ok) cout << "-1";
