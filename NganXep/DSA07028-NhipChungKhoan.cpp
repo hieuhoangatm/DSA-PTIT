e0c325f40fd683707aea20cce9fe9d0a41ef6980
Hoang Dinh Hieu <hieuhoangatm@gmail.com>
2023-04-07 20:10:35 +0700

    Update DSA07028-NhipChungKhoan.cpp

--- NganXep/DSA07028-NhipChungKhoan.cpp
+++ NganXep/DSA07028-NhipChungKhoan.cpp
@@ -5,7 +5,7 @@ int main(){
     int t; cin >> t;
     while(t--){
         int n; cin >> n;
-        int a[n];
+        int a[n+1];
         stack<int> st;
         st.push(0);
         for(int i=1;i<=n;i++){
